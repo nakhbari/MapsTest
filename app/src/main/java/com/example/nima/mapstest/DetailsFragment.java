3d03c985379b099f9273c349a0d77cd49c635587
nakhbari <nnakhbari@gmail.com>
2015-04-02 12:36:20 -0400

    first commit

---
+++ app/src/main/java/com/example/nima/mapstest/DetailsFragment.java
@@ -0,0 +1,7 @@
+package com.example.nima.mapstest;
+
+/**
+ * Created by Nima on 4/2/2015.
+ */
+public class DetailsFragment {
+}
---
+++ app/src/main/java/com/example/nima/mapstest/GMapsFragment.java
@@ -0,0 +1,11 @@
+package com.example.nima.mapstest;
+
+import com.google.android.gms.maps.MapFragment;
+
+/**
+ * Created by Nima on 4/2/2015.
+ */
+public class GMapsFragment extends MapFragment {
+
+
+}
